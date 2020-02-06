d9556b03285dbe9bc794a1b830bc289a36cfbb29
Joacim Breiler <joacim.breiler@tieto.com>
2020-02-06 12:07:09 +0100

    Extended ttl for user token and thing token.

--- src/main/java/com/tietoevry/soilops/service/JwtTokenService.java
+++ src/main/java/com/tietoevry/soilops/service/JwtTokenService.java
@@ -1,5 +1,8 @@
 package com.tietoevry.soilops.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 import com.tietoevry.soilops.model.Roles;
@@ -9,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtTokenService {
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 1000;
+    public static final long JWT_TOKEN_VALIDITY = 7 * 24 * 60 * 60 * 1000;
 
     @Value("${jwt.secret}")
     private String secret;
@@ -39,13 +42,15 @@ public class JwtTokenService {
     }
 
     public String generateThingToken(String username, String uuid) {
+        Instant instant = LocalDateTime.of(2100, 4, 1, 0, 0).atZone(ZoneId.systemDefault()).toInstant();
+
         Map<String, Object> claims = new HashMap<>();
         return Jwts.builder().setClaims(claims)
                 .claim("roles", Collections.singletonList(Roles.THING))
                 .claim("thing", uuid)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .setExpiration(Date.from(instant))
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
     }
